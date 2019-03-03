1248d576717aa3bdae20fc68231590cab25bacca
echavis <echavis@umich.edu>
2019-03-03 01:10:49 -0500

    Add CORS wrapper so requests from interface succeed

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
    :description "FIXME: write description"
    :dependencies [[org.clojure/clojure "1.8.0"]
                   [clj-http "3.9.1"]
+                  [ring-cors "0.1.13"]
                   [metosin/compojure-api "1.1.11"]]
    :ring {:handler interactive-nlp-api.handler/app}
    :uberjar-name "server.jar"
--- src/interactive_nlp_api/handler.clj
+++ src/interactive_nlp_api/handler.clj
@@ -2,6 +2,7 @@
   (:require [compojure.api.sweet :refer :all]
             [ring.util.http-response :refer :all]
             [schema.core :as s]
+            [ring.middleware.cors :refer [wrap-cors]]
             [interactive-nlp-api.openie-facade :as openie-facade]))
 
 (s/defschema UserInput 
@@ -25,14 +26,19 @@
                     :description "Compojure Api example"}
              :tags [{:name "api", :description "apis for collecting information"}]}}}
 
-    (context "/api" []
-      :tags ["api"]
+        (context "/api" []
+          :middleware [#(wrap-cors % :access-control-allow-origin #".*"
+                                     :access-control-allow-headers ["Origin" "X-Requested-With"
+                                                                    "Content-Type" "Accept"]
+                                     :access-control-allow-methods [:get :options :post :delete])]
 
-      (POST "/get-relations" []
-        :return Relations
-        :body [userInput UserInput]
-        :summary "extract relations from text"
-        (ok (map #(s/validate Relation %) 
-                (openie-facade/get-relations (:text userInput))))))))
+          :tags ["api"]
+
+          (POST "/get-relations" []
+            :return Relations
+            :body [userInput UserInput]
+            :summary "extract relations from text"
+            (ok (map #(s/validate Relation %) 
+                  (openie-facade/get-relations (:text userInput))))))))
 
 
