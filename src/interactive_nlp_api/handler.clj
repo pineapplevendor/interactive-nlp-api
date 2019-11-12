85e2a382eed0e5fdd28222ba21185f3d48b595a5
echavis <echavis@umich.edu>
2019-11-11 23:04:39 -0500

    Add operations to track progress in experiment
    Rename benchmark-entry-id to benchmark-sentence-id for clarity

--- src/interactive_nlp_api/handler.clj
+++ src/interactive_nlp_api/handler.clj
@@ -16,11 +16,11 @@
 (s/def ::sentences-to-relations (s/coll-of ::sentence-relations))
 
 (s/def ::experiment-id spec/string?)
-(s/def ::benchmark-entry-id spec/string?)
+(s/def ::benchmark-sentence-id spec/string?)
+(s/def ::benchmark-position (s/and :benchmark-sentence-id ::benchmark-sentence-id))
 (s/def ::benchmark-entry (s/and :sentence spec/string?
-                                :sentence-id spec/string?
+                                :sentence-id ::benchmark-sentence-id
                                 :next-sentence-id spec/string?))
-(s/def ::re-written-sentence spec/string?)
 
 (def app
   (api
@@ -39,20 +39,20 @@
        :return ::sentences-to-relations
        :body [user-input ::user-input]
        (ok (relations-manager/get-relations (:text user-input))))
-     
-     (GET "/get-original-sentence/:benchmark-entry-id" []
-       :path-params [benchmark-entry-id :- ::benchmark-entry-id]
-       :summary "Retrieve the benchmark-entry identified by the given benchmark-entry-id"
+
+     (GET "/get-original-sentence/:benchmark-sentence-id" []
+       :path-params [benchmark-sentence-id :- ::benchmark-sentence-id]
+       :summary "Retrieve the benchmark-entry identified by the given benchmark-sentence-id"
        :return ::benchmark-entry
-       (ok (benchmark-manager/get-benchmark-entry benchmark-entry-id)))
+       (ok (benchmark-manager/get-benchmark-entry benchmark-sentence-id)))
 
      (GET "/get-next-sentence-in-experiment/:experiment-id" []
        :path-params [experiment-id :- ::experiment-id]
        :summary "Get the next benchmark entry in an experiment to re-write"
-       :return ::benchmark-entry-id
+       :return ::benchmark-position
        (ok (benchmark-manager/get-next-sentence-in-experiment experiment-id)))
 
-     (POST "/submit-sentence/:experiment-id/:benchmark-entry-id" []
+     (POST "/submit-sentence/:experiment-id" []
        :path-params [experiment-id :- ::experiment-id]
        :summary "Submit a re-written sentence for a benchmark entry"
        :body [re-written-benchmark-entry :- ::benchmark-entry]
